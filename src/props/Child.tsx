40a403ecf5559f83164c4459aa57a8e8bcffa0c7
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-09 15:17:09 +0300

    add onClick and children props

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,19 +1,39 @@
 // Define props we expect Child component to receive
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-// Destructure color out of props
+// Destructure color and onClick out of props
 // In this definition Typescript does not know this is a
 // React function component. So no access to React component
-// properties eg. Child.displayName
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>;
+// properties eg. Child.displayName or React props children
+// unless defined in our ChildProps interface
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
 
 // Another way of defining a React function component.
 // Component will receive props of type ChildProps
 // Identical React.FunctionComponent
-export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
-  return <div>{color}</div>;
+// Advantage when defining this way is that it expects
+// to receive children prop so can have some content between
+// opening and closing braces when used in different component
+export const ChildAsFC: React.FC<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      {children}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
 };
