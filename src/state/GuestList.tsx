eebc3c916054e941b88d11a01cde1174aab5e9aa
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-09 15:27:41 +0300

    add state and controlled input to guestlist component

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,5 +1,22 @@
+// Import useState hook from React
+import { useState } from 'react';
+
+// Define state property name and its setter
+// Set default value to be empty string
 const GuestList: React.FC = () => {
-  return <div>Guest List</div>;
+  const [name, setName] = useState('');
+
+  // Set up controlled input. On input change receive event object.
+  // Call setName and set name state value as e.target.value which is
+  // input value
+  return (
+    <div>
+      <h3>Guest List</h3>
+
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button>Add Guest</button>
+    </div>
+  );
 };
 
 export default GuestList;
