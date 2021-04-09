0501819d83fb7013d18f18dec1a5ec08c85d2faa
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-09 15:07:58 +0300

    another way of defining React function component

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -4,6 +4,16 @@ interface ChildProps {
 }
 
 // Destructure color out of props
+// In this definition Typescript does not know this is a
+// React function component. So no access to React component
+// properties eg. Child.displayName
 export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>;
+};
+
+// Another way of defining a React function component.
+// Component will receive props of type ChildProps
+// Identical React.FunctionComponent
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
   return <div>{color}</div>;
 };
