9ad2fbe148afeac96ac0f4bb12fcafccf232f4e6
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-09 16:01:44 +0300

    add state and search functionality to usersearch component

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,7 +1,40 @@
 import { useState } from 'react';
 
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 23 },
+  { name: 'Michael', age: 24 },
+  { name: 'Susie', age: 21 },
+];
+
+// Add possible types user object can have
 const UserSearch: React.FC = () => {
-  return <div>User</div>;
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  // Iterate through all users in the array and return user whose
+  // name property matches our name state
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+    setName('');
+  };
+
+  // user && user.name means only try to access user.name property if user is
+  // a truthy value. So only if user is not undefined.
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
 };
 
 export default UserSearch;
