c162885adda93a02dfe752538ecd500039b48e45
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-10 14:08:36 +0300

    finish and test usersearch class component

--- src/classes/UserSearch.tsx
+++ src/classes/UserSearch.tsx
@@ -1,5 +1,57 @@
 import { Component } from 'react';
 
-class UserSearch extends Component {}
+// Define interface for user object
+interface User {
+  name: string;
+  age: number;
+}
+
+// We expect this component to receive as props an array of objects
+// that will have name and age properties {name: string, age: number}[]
+interface UserSearchProps {
+  users: User[];
+}
+
+// Define state User can have
+interface UserSearchState {
+  name: string;
+  user: User | undefined;
+}
+
+// Expects to have type UserSearchProps as props
+class UserSearch extends Component<UserSearchProps> {
+  // Define default values for state
+  state: UserSearchState = { name: '', user: undefined };
+
+  // Iterate through all users in the array and return user whose
+  // name property matches our name state
+  // Use arrow function to bind 'this' and not get undefined
+  onClick = () => {
+    const foundUser = this.props.users.find((user) => {
+      return user.name === this.state.name;
+    });
+    this.setState({ name: '', user: foundUser });
+  };
+
+  render() {
+    // Destructure name and user
+    const { name, user } = this.state;
+
+    return (
+      <div>
+        User Search
+        <input
+          value={name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button onClick={this.onClick}>Find User</button>
+        <div>
+          {user && user.name}
+          {user && user.age}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default UserSearch;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
+import UserSearch from './classes/UserSearch';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 23 },
+  { name: 'Michael', age: 24 },
+  { name: 'Susie', age: 21 },
+];
 
 const App = () => {
   return (
     <div>
-      <EventComponent />
+      <UserSearch users={users} />
     </div>
   );
 };
