37424f563cdffc670a62d7680e601b1cbe4ffad4
Tomi Kivioja <tomi.kivioja@gmail.com>
2021-04-10 14:30:57 +0300

    add auto focus to text input

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
 // import UserSearch from './state/UserSearch';
 import EventComponent from './events/EventComponent';
-import UserSearch from './classes/UserSearch';
+// import UserSearch from './classes/UserSearch';
+import UserSearch from './refs/UserSearch';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -14,7 +15,7 @@ const users = [
 const App = () => {
   return (
     <div>
-      <UserSearch users={users} />
+      <UserSearch />
     </div>
   );
 };
--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,5 +1,5 @@
-// Import useState and useRef hooks
-import { useState, useRef } from 'react';
+// Import useState, useRef and useEffect hooks
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -14,10 +14,29 @@ const users = [
 // Give Ref the HTML element type so it knows the correct type
 // To get a list of all possible HTMLElement interfaces just write
 // HTMLElement and ctrl+click it to see the type definition file
+// Ref is either type HTMLInputElement or null, with default value null.
+// null is needed as Typescript does not know whether the element will be
+// rendered at some point in time or not
 const UserSearch: React.FC = () => {
-  const inputRef = useRef<HTMLInputElement>();
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  // useEffect similar to component lifecycle methods.
+  // Make sure inner function is invoked only one time
+  // Add empty array as second argument so it is called only once when
+  // component is rendered.
+  // Reference to DOM rendered html node or element is available with 'current'
+  // attribute of the ref.
+  // Add type guard to check if there is no inputRef.current, then return early
+  // If there is HTML node rendered on the screen with the ref.current then
+  // as the component is rendered focus mouser pointer automatically to input
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
 
   // Iterate through all users in the array and return user whose
   // name property matches our name state
